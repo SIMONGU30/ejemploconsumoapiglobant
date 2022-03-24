1e706e989aaf10a822682bf96e09f2b3d264fd9a
401 <401@academicos.cesde.edu.co>
2022-03-24 11:10:16 -0500

    se crea ejemplo consumo de api fetch js

---
+++ js/controlador.js
@@ -0,0 +1,24 @@
+import{TOKEN}from "./generarToken.js"
+import{generarURI} from './generadorURI.js'
+import{consumirAPI} from './servicio.js'
+
+//detectar el clic en el boton del formulario
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+
+    evento.preventDefault()
+    
+    let frutaSeleccionada=document.getElementById("fruta").value
+    let URI=generarURI(frutaSeleccionada)
+
+    async function activarAPI(){
+        let datosConsultadosAPI= await consumirAPI(URI,TOKEN)
+        console.log(datosConsultadosAPI)
+    }
+
+    activarAPI()
+    
+    
+
+
+})
