0e96eb099cc7b04a571b8e1af1fe68a2a9f322d3
401 <401@academicos.cesde.edu.co>
2022-03-29 10:31:50 -0500

    se crea solicitud de POST

--- js/generarToken.js
+++ js/generarToken.js
@@ -1,1 +1,30 @@
-export let TOKEN="Bearer BQAcNVeMnvI2KOhkny0ApKrpTPTf0bmHPDMs32ZZomQ5XgCPhXA9iE-QzZ9iDsRZIk4BP8feAtiya45Evlb0_hL9ZJc9SJNTGSXSDISTBGMHMlEB5UMRRVceWmaHFQQNY0CIt74jZ4f4FUvklgldZN2T59zTFW-Zo6Q"
+async function generarToken(){
+
+    let URI="https://accounts.spotify.com/api/token"
+
+    let client_id="client_id=d832b3ad7dec412ab6c2bc9ea2f469cf"
+    let client_secret="client_secret=dea17d1e35f143c688a512b293f69ef9"
+    let grant_type="grant_type=client_credentials"
+
+    let parametros={
+        method:"POST",
+        headers:{"Content-Type":"application/x-www-form-urlencoded"},
+        body:`${client_id}&${client_secret}&${grant_type}`
+    }
+
+    "Bearer BQBgNY0DOHz3kS0YmNa3gXxQMgnRVq49G_44EHgbC_ISWiVTWWyuxeDmy9MnMcNas4YO6nwbzU7XspPHvlD1sLdVgJmeDeJiWIOPPAKGbep5prylMQ_WEzVU4fpZdi9pNV9tm4tUnxlNbNTJ6Po"
+
+    fetch(URI,parametros)
+    .then(function(respuesta){
+        return(respuesta.json())
+    })
+    .then(function(respuesta){
+        console.log(respuesta)
+    })
+    .catch(function(respuesta){
+        console.log(respuesta)
+    })
+
+}
+
+generarToken()
